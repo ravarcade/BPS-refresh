cd08eae05dcb9e5a047558dce7924414ff15c7db
Rafal Janicki <ravarcade@gmail.com>
2021-12-18 17:22:30 +0100

    Added clang-format and c++20 settings

--- executable/ravtest/src/main.cpp
+++ executable/ravtest/src/main.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
 
-int main(int, char**) {
-    std::cout << "Hello, world!\n";
+class CFoo
+{
+public:
+    CFoo() { std::cout << "ctor \n"; }
+
+public:
+    void magic(int arg1, int arg2, int arg3, float someVeryLongName, float someVeryLongName2, float someVeryLone3,
+               float m)
+    {
+        std::cout << arg1 << arg2 << arg3;
+    }
+};
+
+int main(int, char**)
+{
+    for (int i = 0; i < 10; i++) {
+        std::cout << "Hello, world!\n";
+    }
 }
